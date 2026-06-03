1170761b3d7330bd66075de5094473b1649c4341
Reeta Gupta <reetagupta061@gmail.com>
2026-06-03 11:23:12 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (40.28%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -23,3 +23,15 @@ public:
         
     }
 };
+/*
+ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        ListNode* newHead = reverseList(head->next);
+        ListNode* front = head->next;
+        front->next = head;
+        head->next = NULL;
+        return newHead;
+    }
+*/
