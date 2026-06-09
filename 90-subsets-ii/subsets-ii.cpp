417b8406c0624c69f65d0b4615aa2d3b60493703
Reeta Gupta <reetagupta061@gmail.com>
2026-06-09 14:42:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (47.94%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    void findSubsets(int ind, vector<int>& nums, vector<int>& ds, vector<vector<int>>& ans){
+    void subsets(int index, vector<int>& nums, vector<int>& ds, vector<vector<int>>& ans){
         ans.push_back(ds);
-        for(int i = ind; i < nums.size(); i++){
-            if(i != ind && nums[i] == nums[i-1]) continue;
+        for(int i = index; i < nums.size(); i++){
+            if(i > index && nums[i] == nums[i-1]) continue;
             ds.push_back(nums[i]);
-            findSubsets(i+1,nums, ds, ans);
+            subsets(i+1, nums, ds, ans);
             ds.pop_back();
         }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> ds;
-        sort(nums.begin(),nums.end());
-        findSubsets(0,nums,ds,ans);
+        sort(nums.begin(), nums.end());
+        subsets(0, nums, ds, ans);
         return ans;
-
+        
     }
 };
